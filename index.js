3e9a8d97c732d0ed3234f3c01e03a779eadef240
buhi <yinshang1992@gmail.com>
2017-02-14 13:47:09 +0800

    need a index.ts

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "use strict";
 function __export(m) {
-    for (let p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-/**
- * Created by buhi on 2017/2/8.
- */
 __export(require("./build/form"));
